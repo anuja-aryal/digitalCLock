e3e72e9a91256e98fb00a2f4b2de387fd7505c2b
Anuja <aryalanuja99@gmail.com>
2020-10-11 18:33:56 +0545

    final one

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,26 +1,12 @@
-setInterval(showTime, 1000);
-function showTime() {
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
-  am_pm = "AM";
-
-  if (hour > 12) {
-    hour -= 12;
-    am_pm = "PM";
-  }
-  if (hour == 0) {
-    hr = 12;
-    am_pm = "AM";
-  }
-
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
-
-  let currentTime = hour + ":" + min + ":" + sec + am_pm;
-
-  document.getElementById("clock").innerHTML = currentTime;
-}
-showTime();
+var curday = function(sp){
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //As January is 0.
+  var yyyy = today.getFullYear();
+  
+  if(dd<10) dd='0'+dd;
+  if(mm<10) mm='0'+mm;
+  return (mm+sp+dd+sp+yyyy);
+  };
+  console.log(curday('/'));
+  console.log(curday('-'));
